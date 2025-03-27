1d87537f14c5564f5ec87647d0a11f7302b88449
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-03-27 09:34:05 -0300

    Convertendo conexão com o banco para pool de conexões.

--- src/main/java/meubanco/controller/MeuServletJDBC.java
+++ src/main/java/meubanco/controller/MeuServletJDBC.java
@@ -2,11 +2,12 @@ package meubanco.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,35 +16,32 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/consulta_banco")
 public class MeuServletJDBC extends HttpServlet {
-// Informações da conexão JDBC diretamente no código:
-	private static final String URL = "jdbc:mysql://localhost:3307/meu_banco";
-	private static final String USER = "root";
-	private static final String PASSWORD = "root";
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private DataSource dataSource;
 
 	@Override
 	public void init() throws ServletException {
 		try {
-			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {
-			throw new ServletException("Driver JDBC não encontrado", e);
+			InitialContext context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/MeuBanco");
+		} catch (NamingException e) {
+			throw new ServletException("Erro ao configurar JNDI", e);
 		}
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
-		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		try (Connection conn = dataSource.getConnection();
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery("SELECT nome FROM usuario")) {
-			resp.getWriter().println("<h1>Lista de Usuários (via JDBC):</h1>");
+			resp.getWriter().println("<h1>Lista de Usuários:</h1>");
 			resp.getWriter().println("<ul>");
 			while (rs.next()) {
 				resp.getWriter().println("<li>" + rs.getString("nome") + "</li>");
 			}
 			resp.getWriter().println("</ul>");
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new ServletException("Erro SQL ao acessar o banco", e);
 		}
 	}
 }
